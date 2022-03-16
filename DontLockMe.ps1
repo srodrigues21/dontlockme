c00288d3644f65edc2b0b8495faf5b36a541e6e5
sergiorodr <sergio.rodrigues@checkmarx.com>
2022-03-16 15:32:11 +0000

    Support for input settings (runtime and interval)

--- DontLockMe.ps1
+++ DontLockMe.ps1
@@ -1,4 +1,5 @@
 $WShell = New-Object -Com "Wscript.Shell"
+
 Write-Output "________              /\ __    .____                  __                        ._._._."
 Write-Output "\______ \   ____   ___)//  |_  |    |    ____   ____ |  | __   _____   ____     | | | |"
 Write-Output " |    |  \ /  _ \ /    \   __\ |    |   /  _ \_/ ___\|  |/ /  /     \_/ __ \    | | | |"
@@ -6,16 +7,28 @@ Write-Output " |    `   (  <_> )   |  \  |   |    |__(  <_> )  \___|    <  |  Y 
 Write-Output "/_______  /\____/|___|  /__|   |_______ \____/ \___  >__|_ \ |__|_|  /\___  >    ______"
 Write-Output "        \/            \/               \/          \/     \/       \/     \/     \/\/\/"
 Write-Output "                                                                                       "
-Write-Output "version 1.0                                                                            "
+Write-Output "version 1.1                                                                            "
 Write-Output "                                                                                       "
+
+<#inputs#>
+$hours = 8 #in hours
+$sleepInterval = 60 #in seconds
+<#/inputs#>
+
+$numberOfIterations = ($hours*60*60)/$sleepInterval
+$i = 1
+Write-Output "Running $numberOfIterations iterations in $hours hour(s)...                            "
 Write-Output "Log:                                                                                   "
-$i = 1;
-while (1) {
-    Write-Output "  #$i >> Scroll lock sent. Wait another 60 seconds to move again!";
-    $WShell.SendKeys("{SCROLLLOCK}");
-    $WShell.SendKeys("{SCROLLLOCK}");
-    sleep 1;
-    $WShell.SendKeys("{SCROLLLOCK}");
-    sleep 59;
-    $i++;
+
+while ($numberOfIterations -gt $i) {
+    Write-Output "  #$i/$numberOfIterations >> Scroll lock sent. Waiting another $sleepInterval seconds!"
+
+    $WShell.SendKeys("{SCROLLLOCK}")
+    $WShell.SendKeys("{SCROLLLOCK}")
+    
+    sleep 1
+    $WShell.SendKeys("{SCROLLLOCK}")
+    sleep ($sleepInterval - 1)
+
+    $i++
 }
